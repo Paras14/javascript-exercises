6de2f7a396d2de1bc037e66c9cc2804da9b387dc
Paras14 <parasv249@gmail.com>
2022-09-28 08:49:56 +0530

    Add my Solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(stringValue, repeats) {
+    if(repeats<0){
+        return 'ERROR';
+    }
+    else if(repeats === 0){
+        return '';
+    }
+    
+    let tempString = "";
+    for(let i = 0; i < repeats; i++){
+        tempString += stringValue;
+    }
+    return tempString;
 };
 
 // Do not edit below this line
