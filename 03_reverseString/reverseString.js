c94aa229f5760c26e335560cdcd80297221bca73
Paras14 <parasv249@gmail.com>
2022-10-01 23:30:18 +0530

    Add my Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+/*const reverseString = function(str) {
+    var tempStr = '';
+    for(var i=-1; i>=(str.length * (-1)); i--){
+        tempStr += str.substr(i,1);
+    }
+    return tempStr;
+};
+*/
+const reverseString = function(str) {
+    var arrayStr = str.split("");
+    arrayStr.reverse();
+    return arrayStr.join("");
 };
 
 // Do not edit below this line
