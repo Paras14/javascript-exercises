a377e9bfa19147ce238576f3dfb8634fd7aeaa83
Paras14 <parasv249@gmail.com>
2022-10-05 00:31:25 +0530

    Add my Solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  var celValue = ((f-32)*5)/9;
+  return Math.round(celValue*10)/10;
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  var fehValue = ((c*9)/5)+32;
+  return Math.round(fehValue *10)/10;
 
 };
 
