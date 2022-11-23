c5dac13b6e5cfe583eb115c28e0498b6d2cf0091
Paras14 <parasv249@gmail.com>
2022-11-23 18:22:44 +0100

    Add my Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str_test = str.replace(/[^a-z]/gi, '')
+    str_cmp = str_test.split('').reverse().join('');
+    return (str_test === str_cmp);
 };
 
 // Do not edit below this line
