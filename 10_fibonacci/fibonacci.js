237d83b18a9eb4f6ed2f3ace22edd3b56d58b114
Paras14 <parasv249@gmail.com>
2022-11-23 18:47:15 +0100

    Add my Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<=0){
+        return "OOPS";
+    }
+    let a = 1, b = 1, fb = 0;
+    for(let i = 1; i<=num; i++){
+        if(i<=2){
+            fb = 1;
+        }
+        else{
+            a = b;
+            b = fb;
+            fb = a + b;
+        }
+    }
+    return fb;
 };
 
 // Do not edit below this line
